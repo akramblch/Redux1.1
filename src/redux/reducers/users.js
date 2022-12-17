21bf49bad6126c02f384e72207ea77d5be5018a4
Акрам <115643985+akramblch@users.noreply.github.com>
2022-12-17 14:37:10 +0600

    Add files via upload

---
+++ src/redux/reducers/users.js
@@ -0,0 +1,73 @@
+
+
+
+const initialState = {
+    users: [
+        {
+            user: 'Камаль',
+            id: 1
+        },
+        {
+            user: 'РУСЛАНОЧКА',
+            id: 2
+        },
+        {
+            user: 'Тумбочка',
+            id: 3
+        }
+    ],
+    usersCount: 3
+}
+
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case "ADD": {
+            return {
+                ...state,
+                users: [...state.users, {
+                    user: action.user,
+                    id: state.users[state.users.length -1].id + 1
+                }],
+                usersCount: state.usersCount + 1
+            }
+        }
+        case "DELETE": {
+            return {
+                ...state,
+                users: state.users.filter(item => {
+                    return item.id !== action.id
+                }),
+                usersCount: state.usersCount - 1
+            }
+        }
+        case "LIMIT": {
+            return {
+                ...state,
+                users: state.users.filter(item => {
+                    return item.id === 10
+                })
+            }
+        }
+        default: return state
+    }
+}
+
+
+export const addUsers = (user) => {
+    return (dispath) => {
+        return dispath({type: "ADD", user: user})
+    }
+}
+
+export const deleteUsers = (id) => {
+    return (dispath) => {
+        return dispath({type: "DELETE", id})
+    }
+}
+
+export const limitUsers = (id) => {
+    return (dispath) => {
+        return dispath({type: "LIMIT", id})
+    }
+}
